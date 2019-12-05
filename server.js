63f63eaf21160655031c27fbee59e05deea36f98
Steve Brownlee <chortlehoort@gmail.com>
2019-12-05 14:26:15 -0600

    Change order of middleware registration (shrug)

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const path = require('path')
-const dbPath = './api/db.json'
+const dbPath = '/var/data/db.json'
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router(dbPath)
 const middlewares = jsonServer.defaults({ static: "./build" })
 const port = process.env.PORT || 5002
-
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    "/api/animalia": "/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location",
-    "/api/animalia/:id": "/animals/:id?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location"
-
-}))
 
 server.use(middlewares)
 
@@ -27,6 +20,13 @@ server.use((req, res, next) => {
 })
 
 server.use(router)
+
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    "/api/animalia": "/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location",
+    "/api/animalia/:id": "/animals/:id?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location"
+
+}))
 
 server.listen(port, () => {
     console.log(`app running on port ${port}`)
