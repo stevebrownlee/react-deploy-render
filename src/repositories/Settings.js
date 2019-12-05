377c1733708b0e7c3e981018dc6895c77de2859a
Steve Brownlee <chortlehoort@gmail.com>
2019-12-05 11:00:06 -0600

    Correct domain

--- src/repositories/Settings.js
+++ src/repositories/Settings.js
@@ -1,5 +1,5 @@
 export default {
-  remoteURL: "https://nss-kennels-plus.onrender.com/api",
+  remoteURL: "https://nss-kennel-plus.onrender.com/api",
   secretKey: "j249tuhvy8vh9cqujwcnthv9rjqichv293jcb2yuvci1cyubfvuybweu"
 //   remoteURL: "http://kennelapi.nss.team"
 }
