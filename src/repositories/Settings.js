e1dd38047d15773b1c1fabe611bddd5bc0224cdf
Steve Brownlee <chortlehoort@gmail.com>
2019-12-05 10:50:58 -0600

    Intended domain name for settings

--- src/repositories/Settings.js
+++ src/repositories/Settings.js
@@ -1,5 +1,5 @@
 export default {
-  remoteURL: "http://localhost:5002/api",
+  remoteURL: "https://nss-kennels-plus.onrender.com/api",
   secretKey: "j249tuhvy8vh9cqujwcnthv9rjqichv293jcb2yuvci1cyubfvuybweu"
 //   remoteURL: "http://kennelapi.nss.team"
 }
