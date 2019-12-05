021e34fb6dfad8127bca84643d887f5e817ce58b
Steve Brownlee <chortlehoort@gmail.com>
2019-12-05 11:19:15 -0600

    Some debug logging

--- server.js
+++ server.js
@@ -9,14 +9,7 @@ const port = process.env.PORT || 5002;
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     "/api/animalia": "/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location",
-    "/api/animalia/:id": "/animals/:id?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location",
-    "/api/users*": "/600/users$1",
-    "/api/animalOwners*": "/660/animalOwners$1",
-    "/api/treatments*": "/660/treatments$1",
-    "/api/animals*": "/660/animals$1",
-    "/api/owners*": "/660/owners$1",
-    "/api/locations*": "/660/locations$1",
-    "/api/employees*": "/660/employees$1"
+    "/api/animalia/:id": "/animals/:id?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location"
 
 }))
 
@@ -25,9 +18,11 @@ server.use(middlewares);
 server.use((req, res, next) => {
     // use originalUrl since other middleware is likely reassigning req.url
     const isApiRoute = req.originalUrl.includes('/api/');
+    console.log(`isApiRoute: ${isApiRoute}`)
 
     if (isApiRoute) return next();
 
+    console.log("Not API route, serve static file")
     return res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
