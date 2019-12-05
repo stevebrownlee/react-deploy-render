f3728caf99b2ffdbc1522debd963f0194b4c536d
Steve Brownlee <chortlehoort@gmail.com>
2019-12-05 15:15:22 -0600

    Cleaned up

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
-const path = require('path')
-const dbPath = '/var/data/db.json'
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(dbPath)
+const router = jsonServer.router('/var/data/db.json')
+
+//const middlewares = jsonServer.defaults({ noCors: true })
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 5002
+const port = process.env.PORT || 8085
 
-// server.use(middlewares)
-
-// server.use((req, res, next) => {
-//     // use originalUrl since other middleware is likely reassigning req.url
-//     const isApiRoute = req.originalUrl.includes('/api/')
-//     console.log(`isApiRoute: ${isApiRoute} ${req.originalUrl}`)
-
-//     if (isApiRoute) return next()
-
-//     console.log("Not API route, serve static file")
-//     return res.sendFile(path.join(__dirname, './build/index.html'))
-// })
-
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     "/api/animalia": "/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location",
-//     "/api/animalia/:id": "/animals/:id?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location"
-
-// }))
-
+server.use(middlewares)
 server.use(router)
 
 server.listen(port, () => {
-    console.log(`app running on port ${port}`)
+    console.log('JSON Server is running')
 })
