a0806c2721b0d9f30a41863e8f29a57f43e1b4eb
Steve Brownlee <chortlehoort@gmail.com>
2019-12-10 07:38:31 -0600

    Copying db file to disk

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+const initialDbPath = './api/db.json';
+const dbPath = '/var/data/db.json';
+
+const fs = require('fs');
+if (!fs.existsSync(dbPath)) {
+    fs.copyFileSync(initialDbPath, dbPath);
+}
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('/var/data/db.json')
+const router = jsonServer.router(dbPath)
+const middlewares = jsonServer.defaults({
+    static: "./build"
+});
 
-//const middlewares = jsonServer.defaults({ noCors: true })
-const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 8085
 
+server.use(jsonServer.rewriter({
+    "/api/animalia": "/api/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location",
+    "/api/animalia/:id": "/api/animals/:id?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location"
+}))
+
 server.use(middlewares)
-server.use(router)
+server.use('/api', router)
 
 server.listen(port, () => {
     console.log('JSON Server is running')
