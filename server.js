a427f429f761fb87ecea14070431ad432838216a
Steve Brownlee <chortlehoort@gmail.com>
2019-12-10 08:21:54 -0600

    Can't use wildcard

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ const port = process.env.PORT || 8085
 
 server.use(middlewares)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
+    "/api/users*": "/users$1",
+    "/api/animalOwners*": "/animalOwners$1",
+    "/api/treatments*": "/treatments$1",
+    "/api/animals*": "/animals$1",
+    "/api/locations*": "/locations$1",
+    "/api/employees*": "/employees$1",
     "/api/animalia": "/animals?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location",
     "/api/animalia/:id": "/animals/:id?_expand=employee&_sort=employee.id&_embed=treatments&_expand=location"
 }))
